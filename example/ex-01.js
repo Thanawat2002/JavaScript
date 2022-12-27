328bc4f645cf867821f8b18fcc63faeb04dd53a1
Thanawat2002 <109454113+Thanawat2002@users.noreply.github.com>
2022-12-27 23:54:19 +0700

    Lecture-03

---
+++ example/ex-01.js
@@ -0,0 +1,6 @@
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+function findPerimeter(length, width) {
+    return 2 * (length + width);
+}
+
+console.log(findPerimeter(6, 7));
---
+++ example/ex-02.js
@@ -0,0 +1,7 @@
+// Create a function that takes an equation (e.g. "1+1"), and returns the answer.
+
+function equation(answer) {
+    return (eval(answer));
+}
+
+console.log(equation(7 * 4 - 2));
