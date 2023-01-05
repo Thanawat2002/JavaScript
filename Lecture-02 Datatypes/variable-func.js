06bb87fab95fee2266054f954cc5d3cb7f4329d4
Thanawat2002 <thanawat.220222@gmail.com>
2023-01-05 10:04:35 +0700

    Lecture-02

--- Lecture-02 Datatypes/variable-func.js
+++ Lecture-02 Datatypes/variable-func.js
@@ -1,9 +1,9 @@
 let plusFive = (number) => {
     return number + 5;
 };
-
+// f is assigned the value of plusFive
 let f = plusFive;
 
-console.log(plusFive(3));
+console.log(plusFive(3)); //8
 
 console.log(f(9));
