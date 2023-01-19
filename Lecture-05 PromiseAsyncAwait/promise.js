df3ea592276c22804716b30a635ce118fc2913cb
Thanawat2002 <thanawat.220222@gmail.com>
2023-01-19 08:52:44 +0700

    Lecture-05

--- Lecture-05 PromiseAsyncAwait/promise.js
+++ Lecture-05 PromiseAsyncAwait/promise.js
@@ -1,15 +1,34 @@
 function promiseTimeout(ms) {
+    console.log('Asynchoronous work');
     return new Promise((resolve, reject) => {
         setTimeout(resolve, ms);
     });
 }
 
-async function run() {
-    // logic
-    console.log('Start');
-    // try take of await and compare
-    await promiseTimeout(2000);
-    console.log('Stop!!');
-}
+promiseTimeout(2000)
+    .then(() => {
+        console.log('Done!!');
+        return promiseTimeout(1000);
+    })
+    .then(() => {
+        console.log('Also Done!!');
+        return Promise.resolve(42);
+    })
+    .then((result) => {
+        console.log(result);
+    })
+    .catch(() => {
+        console.log('Error');
+    });
 
-run();
+
+
+// async function run() {
+//     // logic
+//     console.log('Start');
+//     // try take of await and compare
+//     await promiseTimeout(2000);
+//     console.log('Stop!!');
+// }
+
+// run();
